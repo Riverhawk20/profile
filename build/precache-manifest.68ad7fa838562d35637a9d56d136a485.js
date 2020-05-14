e97b0dc1349bfb816ed1e111d462965a75f9d49e
Riverhawk20 <cadenmrice@gmail.com>
2020-05-14 16:55:32 -0400

    Update maybe

---
+++ build/precache-manifest.68ad7fa838562d35637a9d56d136a485.js
@@ -0,0 +1,110 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "cbd12770042229ef1f3e49bb2e0c9638",
+    "url": "/profile/index.html"
+  },
+  {
+    "revision": "b7d28fcfb9b59f381aa3",
+    "url": "/profile/static/css/main.8530a393.chunk.css"
+  },
+  {
+    "revision": "4d92080701b9097b68f2",
+    "url": "/profile/static/js/2.5007fdf3.chunk.js"
+  },
+  {
+    "revision": "99bd0487192ec9e7d9ee8fbbd91ee444",
+    "url": "/profile/static/js/2.5007fdf3.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "b7d28fcfb9b59f381aa3",
+    "url": "/profile/static/js/main.5d1ec860.chunk.js"
+  },
+  {
+    "revision": "b5a7d259590e62b51bcc",
+    "url": "/profile/static/js/runtime-main.3026efc4.js"
+  },
+  {
+    "revision": "e3b91280dd9ac7ddbfc2c0e78a94e48a",
+    "url": "/profile/static/media/Dino1.e3b91280.png"
+  },
+  {
+    "revision": "130d23464cfe5bc1f62ac7595228a16d",
+    "url": "/profile/static/media/Dino2.130d2346.png"
+  },
+  {
+    "revision": "f3d4eab1fcf62d4df87d7a22870a8cd8",
+    "url": "/profile/static/media/android.f3d4eab1.png"
+  },
+  {
+    "revision": "da53dc3418ca99ce8fec3846274d9cb8",
+    "url": "/profile/static/media/bootstrap.da53dc34.png"
+  },
+  {
+    "revision": "8f42e018a9340b6e4e20161b06525308",
+    "url": "/profile/static/media/c++.8f42e018.png"
+  },
+  {
+    "revision": "dad5aa0b295619c1d78034a509647e09",
+    "url": "/profile/static/media/fcc.dad5aa0b.JPG"
+  },
+  {
+    "revision": "ff9bff94378493e07167ac9edfd698d3",
+    "url": "/profile/static/media/fcc2.ff9bff94.JPG"
+  },
+  {
+    "revision": "f794246a69ff793397575a0f6591f88d",
+    "url": "/profile/static/media/firebase.f794246a.jpg"
+  },
+  {
+    "revision": "a3af0655fc9a481401550b5be0e86cab",
+    "url": "/profile/static/media/jquery.a3af0655.png"
+  },
+  {
+    "revision": "487e1d7ba36a5dcef8d3f54c80b075c4",
+    "url": "/profile/static/media/ptcb1.487e1d7b.png"
+  },
+  {
+    "revision": "893ef79e9729c7d0d3b406aa1a36e7ff",
+    "url": "/profile/static/media/ptcb2.893ef79e.png"
+  },
+  {
+    "revision": "85360e7b7b992c084846b5905d787a16",
+    "url": "/profile/static/media/ptcb3.85360e7b.png"
+  },
+  {
+    "revision": "21cef5f5ff8f0a831ed1692e91e7154a",
+    "url": "/profile/static/media/react.21cef5f5.png"
+  },
+  {
+    "revision": "a1e8f6456906f568fe54f40be5bbd97d",
+    "url": "/profile/static/media/reaction1.a1e8f645.png"
+  },
+  {
+    "revision": "b13b875665252a0c9aed456aecd7c422",
+    "url": "/profile/static/media/reaction2.b13b8756.png"
+  },
+  {
+    "revision": "b46c285c2835ef41723c92a2e2d907db",
+    "url": "/profile/static/media/robot1.b46c285c.png"
+  },
+  {
+    "revision": "07c3b0e6ea63ce6546a031270476763f",
+    "url": "/profile/static/media/sql.07c3b0e6.jpeg"
+  },
+  {
+    "revision": "3d7c84d71630eb0eafefb5471662caf0",
+    "url": "/profile/static/media/unity.3d7c84d7.jpg"
+  },
+  {
+    "revision": "d83cd0946760a33c5fbede26bf72df1b",
+    "url": "/profile/static/media/valorant1.d83cd094.png"
+  },
+  {
+    "revision": "c40d795452d4d6b74cc628eff2df35c2",
+    "url": "/profile/static/media/valorant2.c40d7954.png"
+  },
+  {
+    "revision": "a865428c423555d3b264019041535778",
+    "url": "/profile/static/media/valorant3.a865428c.png"
+  }
+]);
--- build/service-worker.js
+++ build/service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "./precache-manifest.3f848bd9296ff43727e71165aae1f943.js"
+  "/profile/precache-manifest.68ad7fa838562d35637a9d56d136a485.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -33,7 +33,7 @@ workbox.core.clientsClaim();
 self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("./index.html"), {
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/profile/index.html"), {
   
   blacklist: [/^\/_/,/\/[^/?]+\.[^/]+$/],
 });
